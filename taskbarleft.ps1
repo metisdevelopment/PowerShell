10a80babee10aa116798a0c08fa1d2237129dfd5
Tom @ Metis <128331110+metisdevelopment@users.noreply.github.com>
2023-12-12 15:08:07 +0000

    Align Taskbar Left via Intune

---
+++ taskbarleft.ps1
@@ -0,0 +1,7 @@
+$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+
+$Al = "TaskbarAl" # Shift Start Menu Left
+
+$value = "0"
+
+New-ItemProperty -Path $registryPath -Name $Al -Value $value -PropertyType DWORD -Force -ErrorAction Ignore
