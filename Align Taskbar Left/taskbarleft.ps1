b1ea131a184f2c395d5293b2271f16aea7667265
Tom @ Metis <128331110+metisdevelopment@users.noreply.github.com>
2023-12-18 23:16:48 +0000

    Update taskbarleft.ps1
    Adjustment to comments in the script. Closed it up now that's complete.

--- Align Taskbar Left/taskbarleft.ps1
+++ Align Taskbar Left/taskbarleft.ps1
@@ -1,7 +1,4 @@
-$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
-
-$Al = "TaskbarAl" # Shift Start Menu Left
-
-$value = "0"
-
+$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" # Path for the registry key to be deployed
+$Al = "TaskbarAl" # Align this name with your documentation for easy tracing
+$value = "0" # Aligns the taskbar to the left in the DWORD registry key
 New-ItemProperty -Path $registryPath -Name $Al -Value $value -PropertyType DWORD -Force -ErrorAction Ignore
